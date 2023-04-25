026d984eab7e126f8ec542a888846dfdf53accaf
xiaoshuangxiaoshuang <80088258+mxweng@users.noreply.github.com>
2023-04-25 23:19:28 +0800

    add

---
+++ spectral method/chebPy.py
@@ -0,0 +1,17 @@
+from numpy import pi,cos,arange,ones,tile,dot,eye,diag
+
+def cheb(N):
+    '''Chebushev polynomial differentiation matrix.
+       Ref.: Trefethen's 'Spectral Methods in MATLAB' book.
+    '''
+    x      = cos(pi*arange(0,N+1)/N)
+    if N%2 == 0:
+        x[N//2] = 0.0 # only when N is even!
+    c      = ones(N+1); c[0] = 2.0; c[N] = 2.0
+    c      = c * (-1.0)**arange(0,N+1)
+    c      = c.reshape(N+1,1)
+    X      = tile(x.reshape(N+1,1), (1,N+1))
+    dX     = X - X.T
+    D      = dot(c, 1.0/c.T) / (dX+eye(N+1))
+    D      = D - diag( D.sum(axis=1) )
+    return D,x
